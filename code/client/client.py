f9557a1c0b79bfa3f2db20a230f3bc6b89e8aa01
jeramyleon <jeramyleon@gmail.com>
2022-11-19 17:35:06 -0500

    refactoring code to implements classes

--- code/client/client.py
+++ code/client/client.py
@@ -1,18 +1,77 @@
 import socket 
+import os 
 
+"""
 s = socket.socket()
+host = '127.0.0.1'
 port = 12345                   # define port we want to connect to
-s.connect(('127.0.0.1', port)) # connect to server on local computer 
+s.connect((host, port)) # connect to server on local computer 
 print("connected")
 
-filename = input(str("Please enter a filename for the incoming file: "))
-file = open(filename, 'wb') # open new file for writing in binary mode 
-file_data = s.recv(1024) # receive file data from server 
-file.write(file_data) # write file data to file 
-file.close() 
-print("File has been received successfully")
+if not os.path.isfile('transferfile.txt'):
+    filename = input(str("Please enter a filename for the incoming file: "))
+    file = open(filename, 'wb') # open new file for writing in binary mode 
+    file_data = s.recv(1024)    # receive file data from server 
+    file.write(file_data)       # write file data to file 
+    file.close() 
+    print("File has been received successfully")
 
+elif os.path.isfile('transferfile.txt'):
+    file = open('transferfile.txt', 'rb') # open transferfile.txt for reading in binary mode
+    file_data = file.read(1024) # returns specified number of bytes from the file
+    s.send(file_data) # send file data to connected socket
+    file.close()
+    print("Data has been transmitted successfully")
 
+    os.remove('transferfile.txt') # delete file since we've already transferred data
+"""
 
+class Client: 
+    def __init__(self, host, port):
+        """Initialize client object and define which host and port we want to connect to."""
 
+        self.s = socket.socket()
+        self.host = host 
+        self.port = port
 
+    def connect(self):
+        """Connect to specified host and port."""
+
+        self.s.connect((self.host, self.port))
+        print("connected")
+
+        return True   
+
+    def sendFile(self, filename):
+        """Send file over network."""
+
+        file = open(filename, 'rb') # open file for reading in binary mode
+        file_data = file.read(1024) # returns specified number of bytes from file
+        self.s.send(file_data) # send file data to connected socket
+        file.close() 
+
+        os.remove(filename) # delete file since we've already transferred
+        print("File sent successfully.") 
+        return True
+    
+    def receiveFile(self):
+        """Receive file over network."""
+
+        filename = input(str("Please enter a filename for the incoming file: "))
+        file = open(filename, 'wb') # open new file for writing in binary mode 
+        file_data = self.s.recv(1024)    # receive file data from server 
+        file.write(file_data)       # write file data to file 
+        file.close() 
+
+        print("File received successfully.")
+        return True
+
+ 
+client = Client('127.0.0.1', 12345)
+client.connect()
+
+if os.path.isfile('transferfile.txt'):
+    client.sendFile('transferfile.txt')
+else: 
+    client.receiveFile()
+    
--- code/server/server.py
+++ code/server/server.py
@@ -1,6 +1,7 @@
 import socket
 import os 
 
+"""
 s = socket.socket()
 
 host = '' # empty string means server can listen to requests coming from other computers on network 
@@ -13,17 +14,50 @@ print("Socket is listening....")
 c, addr = s.accept() # Establish connection with client 
 print(addr, "has connected to the server")
 
-file = open('transferfile.txt', 'rb') # open transferfile.txt for reading in binary mode
-file_data = file.read(1024) # returns specified number of bytes from the file
-c.send(file_data) # send file data to connected socket
-file.close()
-print("Data has been transmitted successfully")
+if os.path.isfile('transferfile.txt'): # if transferfile.txt exists in directory
 
-os.remove('transferfile.txt') # delete file since we've alread transferred data
+    file = open('transferfile.txt', 'rb') # open transferfile.txt for reading in binary mode
+    file_data = file.read(1024) # returns specified number of bytes from the file
+    c.send(file_data) # send file data to connected socket
+    file.close()
+    print("Data has been transmitted successfully")
 
+    os.remove('transferfile.txt') # delete file since we've already transferred data
+else:
 
+    filename = input(str("Please enter a filename for the incoming file: "))
+    file = open(filename, 'wb') # open new file for writing in binary mode 
+    file_data = c.recv(1024)    # receive file data  
+    file.write(file_data)       # write file data to file 
+    file.close() 
+    print("File has been received successfully")
+"""
 
+class Server:
+    def __init__(self, host, port):
+        """Initialize server."""
 
- 
+        self.s = socket.socket()
+        self.host = host 
+        self.port = port 
+        self.s.bind((self.host, self.port))
 
+    def start(self):
+        """Listen for connections and establish connections."""
+        
+        self.s.listen(5) # Accepts 5 connections.
+        print("Server is listening...")
+
+        c, addr = self.s.accept() # Establish connection with client.
+        print(addr, "has connected to the server.")
+
+        return True 
     
+    def sendFile(self):
+        pass 
+
+    def receiveFile(self):
+        pass 
+
+server = Server('', 12345)
+server.start()
